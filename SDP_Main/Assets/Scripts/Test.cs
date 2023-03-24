d80151305784717d599d6d31d7961f19b80197b2
LeroyJohnKnight <coreyk@windowslive.com>
2023-03-24 19:21:29 +1300

    Setup Main

---
+++ SDP_Main/Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("TESTIES");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
