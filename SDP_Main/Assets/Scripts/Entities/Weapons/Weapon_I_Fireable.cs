2cfd3c5b5e4a61a25ee59e50059243e988222691
gtk2105 <106717871+gtk2105@users.noreply.github.com>
2023-04-17 18:18:11 +1200

    Revert "Merge branch 'main' into Dion---VoiceChat"
    This reverts commit 19379c6b540bb69c7a86b107e83152afe5d2ddcc, reversing
    changes made to 07e925f93b3e5ddac2b1d8b5ec9b9b7f1901d4e7.

--- SDP_Main/Assets/Scripts/Entities/Weapons/Weapon_I_Fireable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Weapon_I_Fireable
-{
-    public void Fire(Transform origin);
-    public void Hit(Transform origin);
-}
