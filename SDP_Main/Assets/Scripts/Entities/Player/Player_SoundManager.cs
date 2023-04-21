5d49bcc83ac6ed8211f89801ada2e9a86fe6a378
gtk2105 <106717871+gtk2105@users.noreply.github.com>
2023-04-21 17:22:52 +1200

    Revert "Added Multiplayer Proximity Voice Chat" (#12)

--- SDP_Main/Assets/Scripts/Entities/Player/Player_SoundManager.cs
+++ SDP_Main/Assets/Scripts/Entities/Player/Player_SoundManager.cs
@@ -24,30 +24,8 @@ public class Player_SoundManager : MonoBehaviour
     [Tooltip("Sound played when taking damage froma fall")]
     public AudioClip FallDamageSfx;
 
-    public bool proximityVoiceMute;
 
     float m_FootstepDistanceCounter;
-
-
-    private void Awake()
-    {
-        proximityVoiceMute = false;
-    }
-
-    private void Update()
-    {
-        if (transform.parent.GetComponent<Player_InputManager>().GetVoiceMuteButtonIsPressed())
-            OnMuteButtonPressed();
-    }
-
-    private void OnMuteButtonPressed()
-    {
-        proximityVoiceMute = !proximityVoiceMute;
-
-        transform.GetChild(0).GetComponent<AudioSource>().mute = proximityVoiceMute;
-        Debug.Log(transform.GetChild(0));
-        Debug.Log(transform.GetChild(0).GetComponent<AudioSource>());
-    }
 
     internal void PlayFallDamage()
     {
---
+++ SDP_Main/Assets/Scripts/Entities/Weapons/Weapon_WeaponController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(Weapon_ProjectileManager))]
+public class Weapon_WeaponController : MonoBehaviour
+{
+    public Player_InputManager inputHandler;
+    internal Weapon_ProjectileManager weaponProjectileMananger;
+
+    [SerializeField] internal PhotonView PhotonView;
+    internal bool IsMultiplayer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+
+        inputHandler = GetComponentInParent<Player_InputManager>();
+        weaponProjectileMananger = GetComponent<Weapon_ProjectileManager>();
+    }
+    void Start()
+    {
+        IsMultiplayer = Game_RuntimeData.isMultiplayer;
+      //  PhotonView = FindObj
+        //initiating bullet counts.
+        weaponProjectileMananger.InitBullets();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PhotonView.IsMine)
+        {
+            return;
+           // Destroy(GetComponentInChildren<Camera>().gameObject);
+        }
+
+        weaponProjectileMananger.UpdateChildTransform();
+     
+        
+    }
+}
