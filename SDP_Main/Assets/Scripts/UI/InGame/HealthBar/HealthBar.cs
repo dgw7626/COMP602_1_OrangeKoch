ea5d5cd1d93db36d48a523ef8c8cd9721130e485
persona2490 <114039152+persona2490@users.noreply.github.com>
2023-05-04 01:24:49 +1200

    Kevin in-game player UI and Tutorial map (#34)
    * Fix Nickname error
    
    * test
    
    * Expand IgameMode, implement in GameMode_Standard
    
    * Fix runtime list of Players, add callbacks for player Enter and Player Leave room
    
    * Modify Player Prefab Model
    
    * Created MultiplayerData structs
    
    * Update PhotonServerSettings.asset
    
    * Commented out PlayerController - shoot
    
    * Revert "Merge branch 'Corey---GameState_GameMode' into Kevin-PlayerUI"
    
    This reverts commit e7dd0bc8838f2dbadf339d61cabc2f8f06bdefda, reversing
    changes made to 4debd126d8c23aad4481bc0e7d06afac8692aca0.
    
    * Player Health Bar UI
    
    - Health bar will change its color based on the health amount.
    - Change the health decrease speed.
    
    * Begin building a simple tutorial map
    
    - add move tutorial text UI
    - add jump tutorial text UI
    
    * Fix some unsaved problem
    
    -Push the latest version, because last time did not  successfully push everything I have done, and fix some small problem
    
    * Fix  player prefab merging problem
    
    - Fixing  player prefab merging problem
    
    * Updated Player Prefab
    
    - Updated Player Tutorial UI
    
    * Cut unnecessary  files
    
    -Cut unnecessary  files
    
    * Cut useless file
    
    Cut useless file
    
    ---------
    
    Co-authored-by: LeroyJohnKnight <coreyk@windowslive.com>
    Co-authored-by: _Leroy_Jenkins <89508010+CoreyJohnKight@users.noreply.github.com>

--- SDP_Main/Assets/Scripts/UI/InGame/HealthBar/HealthBar.cs
+++ SDP_Main/Assets/Scripts/UI/InGame/HealthBar/HealthBar.cs
@@ -7,15 +7,19 @@ public class HealthBar : MonoBehaviour
 {
     // Start is called before the first frame update
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(float health)
     {
         slider.value = health;
+        fill.color= gradient.Evaluate(slider.normalizedValue);
     }
 }
