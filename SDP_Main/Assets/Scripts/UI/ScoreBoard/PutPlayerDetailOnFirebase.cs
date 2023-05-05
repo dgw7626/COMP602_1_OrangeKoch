0c2226dc8088a0bbaf8e491c8e654aa83ee2e626
munishk686 <munishk686@gmail.com>
2023-05-05 15:48:19 +1200

    Update ScoreboardUI
    - Deleted the won item from scoreboard
    - In the main game. scoreboard pop up
      when press tab button.

--- SDP_Main/Assets/Scripts/UI/ScoreBoard/PutPlayerDetailOnFirebase.cs
+++ SDP_Main/Assets/Scripts/UI/ScoreBoard/PutPlayerDetailOnFirebase.cs
@@ -6,15 +6,22 @@ using  Proyecto26;
 
 public class PutPlayerDetailOnFirebase : MonoBehaviour 
 {
-     ScoreManager scoreMan1 = new ScoreManager("Jay", 5, 5, 5);
-     ScoreManager scoreMan2 = new ScoreManager("John", 10, 10, 10);
+    ScoreManager scoreMan1 = new ScoreManager("hg", 5, 0);
+    ScoreManager scoreMan2 = new ScoreManager("John", 10, 10);
 
  // The start methode put the player detail on Firebase with help of RestClient.   
     void Start()
     {
-         RestClient.Put("https://project-10bbb-default-rtdb.firebaseio.com/player1.json", scoreMan1);
-         RestClient.Put("https://project-10bbb-default-rtdb.firebaseio.com/player2.json", scoreMan2);
-         //RestClient.Delete("https://project-10bbb-default-rtdb.firebaseio.com/.json");
+     RestClient.Put("https://project-10bbb-default-rtdb.firebaseio.com/player1.json", scoreMan1);
+     RestClient.Put("https://project-10bbb-default-rtdb.firebaseio.com/player2.json", scoreMan2);    
     }
+
+     void Update()
+     {
+          //ScoreManager("Sky", 5, 5);
+          //ScoreManager("Harry", 10, 10);
+          
+         //RestClient.Delete("https://project-10bbb-default-rtdb.firebaseio.com/.json");
+     }
     
 }
--- SDP_Main/Assets/Scripts/UI/ScoreBoard/ScoreBoard.cs
+++ SDP_Main/Assets/Scripts/UI/ScoreBoard/ScoreBoard.cs
@@ -8,23 +8,25 @@ using System.Net.Mime;
 
 public class ScoreBoard : MonoBehaviour
 {
+    
 // Assiging the Text attribute.
     public Text playerNameText_1;
     public Text playerKillsText_1;
     public Text playerDeathsText_1;
-    public Text playerWonText_1;
+    //public Text playerWonText_1;
 
     public Text playerNameText_2;
     public Text playerKillsText_2;
     public Text playerDeathsText_2;
-    public Text playerWonText_2;
-    
+    //public Text playerWonText_2;
+    internal bool _activeSelf;
+
    PlayerDetail playerdetail = new PlayerDetail();
        
      public static string playerName;
      public static int playerKills;
      public static int playerDeaths;
-     public static int playerWon;
+     //public static int playerWon;
 
 // This methode update player 1 detail print on Scoreboard.
      public void UpdatePlayerDetail_1()
@@ -32,7 +34,7 @@ public class ScoreBoard : MonoBehaviour
       playerNameText_1.text = playerdetail.playerName;
       playerKillsText_1.text = playerdetail.kills.ToString();
       playerDeathsText_1.text = playerdetail.deaths.ToString();
-      playerWonText_1.text = playerdetail.won.ToString();
+      //playerWonText_1.text = playerdetail.won.ToString();
     }
 
 // This methode update player 2 detail print on Scoreboard.
@@ -41,9 +43,17 @@ public class ScoreBoard : MonoBehaviour
       playerNameText_2.text = playerdetail.playerName;
       playerKillsText_2 .text = playerdetail.kills.ToString();
       playerDeathsText_2.text = playerdetail.deaths.ToString();
-      playerWonText_2.text = playerdetail.won.ToString();
+      //playerWonText_2.text = playerdetail.won.ToString();
     }
-
+    private void Start(){
+      this._activeSelf = false;
+      transform.gameObject.SetActive(this._activeSelf);
+    }
+    public void GetScoreboard(){
+      this._activeSelf = !_activeSelf;
+      transform.gameObject.SetActive(this._activeSelf);
+      return;
+    }
    private void Update()
    {
      RestClient.Get<PlayerDetail>("https://project-10bbb-default-rtdb.firebaseio.com/" + "/player1" +".json").Then(response =>
