3e2bd54e3c83f8bb9699a0d2d4aae534c99ba693
gtk2105 <106717871+gtk2105@users.noreply.github.com>
2023-05-20 12:37:04 +1200

    Bug Fix Mouse Sensitivity
    Bug Fix Mouse sensitivity not working when set below 1/3 of slider.
    Updated PlayerSettings set methods
    Updated MenuOptions set calls
    Refactored Local_GameSettings Name to Menu_OptionsController
    Removed MenuOption Cast to Int

--- SDP_Main/Assets/Scripts/Entities/Player/Player_Settings.cs
+++ SDP_Main/Assets/Scripts/Entities/Player/Player_Settings.cs
@@ -10,15 +10,31 @@ public class Player_Settings : MonoBehaviour
     {
          invertMouseYAxis = !invertMouseYAxis;
     }
+    public void setInvertMouseYAxis(bool invert)
+    {
+        //Check bounds
+        if (invert || !invert)
+        {
+            invertMouseYAxis = invert;
+            Debug.Log("Invert Mouse Y Axis is set to: " + invertMouseYAxis);
+        }
+    }
     public void setMouseSpeed(float speedMultiplier)
     {
         //Check bounds
-        if(speedMultiplier >= 0.5f && speedMultiplier <= 2.0f)
+        if(speedMultiplier >= 0.25f && speedMultiplier <= 2.0f)
+        {
             lookSensitivity = speedMultiplier;
+            Debug.Log("Look Sensitivity Multiplier Set to: "+lookSensitivity);
+        }
     }
     public void setGlobalVolume(float volume)
     {
-        globalVolume = volume;
+        //Check bounds
+        if (volume >= 0.0001f && volume <= 1.0f)
+        {
+            globalVolume = volume;
+            Debug.Log("Global Volume Set to: " + globalVolume);
+        }
     }
-    
 }
--- SDP_Main/Assets/Scripts/Menu/Menu_OptionsController.cs
+++ SDP_Main/Assets/Scripts/Menu/Menu_OptionsController.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Local_GameSettings : MonoBehaviour
+public class Menu_OptionsController : MonoBehaviour
 {
     [Header("Sliders & Toggles")]
     [SerializeField] private Slider mouseSensitivitySlider;
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private Toggle invertYAxisToggle;
 
-    private const string _sliderMouseSensitivity = "sliderMouseSensitivity";
 
+    /// <summary>
+    /// Starts a listener when the Options Menu is opened to listen for changes on Sliders and Toggles
+    /// </summary>
     private void Start()
     {
+        //Mouse Sensitivity Slider
         mouseSensitivitySlider = GetComponentsInChildren<Slider>()[1];
         mouseSensitivitySlider.onValueChanged.AddListener((value) => {
-            Game_RuntimeData.playerSettings.lookSensitivity = (int) value;
+            Game_RuntimeData.playerSettings.setMouseSpeed(value);
         });
 
-
+        //Invert Mouse Y Axis Toggle 
         invertYAxisToggle = GetComponentsInChildren<Toggle>()[0];
         invertYAxisToggle.onValueChanged.AddListener((IsOn) => {
-            Game_RuntimeData.playerSettings.invertMouseYAxis = IsOn;
+            Game_RuntimeData.playerSettings.setInvertMouseYAxis(IsOn);
         });
 
-        //Volume Control
+        //Global Volume Control
         volumeSlider = GetComponentsInChildren<Slider>()[0];
         volumeSlider.onValueChanged.AddListener((value) => {
-            Game_RuntimeData.playerSettings.globalVolume = value;
+            Game_RuntimeData.playerSettings.setGlobalVolume(value);
         });
     }
 }
