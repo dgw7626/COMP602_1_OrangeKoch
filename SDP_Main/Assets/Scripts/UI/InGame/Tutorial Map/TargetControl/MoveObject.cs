bc3ec9d2c88f2a1b9ebc9c7d80b81ddc500fe7ed
persona2490 <114039152+persona2490@users.noreply.github.com>
2023-06-02 05:09:23 +1200

    Update the tutorial map
    - fix the folder position problem
    - make target movable
    - add target respawn function

---
+++ SDP_Main/Assets/Scripts/UI/InGame/Tutorial Map/TargetControl/MoveObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public Transform startPosition;
+    public Transform endPosition; 
+    public float moveSpeed = 2f;
+
+    private bool movingToEnd = true;
+
+
+    private void Update()
+    {
+        if (movingToEnd)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPosition.position, moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, endPosition.position) < 0.01f)
+            {
+                movingToEnd = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPosition.position, moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, startPosition.position) < 0.01f)
+            {
+                movingToEnd = true;
+            }
+        }
+    }
+}
---
+++ SDP_Main/Assets/Scripts/UI/InGame/Tutorial Map/TargetControl/TargetRespawn.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetRespawn : MonoBehaviour
+{
+    public float respawnTime = 3f; // spawn time
+    public Vector3 respawnPosition; // spawn position
+
+    private bool isRespawning = false;
+
+    private void Start()
+    {
+        // Set the initial spawn position to the current position
+        respawnPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isRespawning)
+        {
+            //Destroy(gameObject);
+            RespawnDelay();
+        }
+    }
+
+    public void RespawnDelay()
+    {
+        isRespawning = true;
+        // Hide object
+        gameObject.SetActive(false);
+        Debug.Log("2s");
+        // Wait for a moment before activating the object
+        Invoke(nameof(Respawn), respawnTime);
+        Debug.Log("3s");
+    }
+
+    private void Respawn()
+    {
+        Debug.Log("3.5s");
+
+        // Set the spawn position for new objects
+        transform.position = respawnPosition;
+        Debug.Log("4s");
+
+        // Activate the object
+        gameObject.SetActive(true);
+        Debug.Log("5s");
+
+        isRespawning = false;
+    }
+}
