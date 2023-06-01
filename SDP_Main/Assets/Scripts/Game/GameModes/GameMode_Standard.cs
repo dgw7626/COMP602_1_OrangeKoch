5a63763b831266e7834e99df89839cc83067cbb8
persona2490 <114039152+persona2490@users.noreply.github.com>
2023-06-01 15:52:09 +1200

    Squashed commit of the following:
    commit ca4c2a0d02d387fc1ea8da58e4c17d833916b442
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Thu Jun 1 01:20:20 2023 +1200
    
        add comment
    
        add comment in  multiplayerEntity and GameMode standard for my part
    
    commit 4146d3074ca7fdea0cb9ffc5f85fb9ee1ea2d045
    Author: gtk2105 <106717871+gtk2105@users.noreply.github.com>
    Date:   Wed May 31 22:36:20 2023 +1200
    
        Updated Player Respawn
    
        - Added PunRPC to implement Invincibility
        - Added code to replenish ammunition
        - Added invincibility variable for player health
    
    commit f936c57fb1489e21592c71622b0a1ec5d3a70c92
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Wed May 31 22:05:18 2023 +1200
    
        Update Player_Health.cs
    
    commit 5460c9497b61ce5b59ac901242760b807aea0261
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Wed May 31 22:02:45 2023 +1200
    
        keep develop player respawn
    
        keep develop player respawn
    
    commit 58d4ca11ad4f057e37066c915584e6836aeedea9
    Merge: d91a91a ed336da
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Wed May 31 19:30:28 2023 +1200
    
        Merge branch 'main' into Kevin---PlayerRespawn
    
    commit d91a91afce5fcf971dc3d2f50797435354d18181
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Wed May 31 19:30:09 2023 +1200
    
        prepare for update
    
    commit 025c8546956f7a766d3dfb296adcc9096663aaee
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Wed May 31 18:29:16 2023 +1200
    
        fix the error
    
    commit 75cd19b765bf7184fa72499d491f42812ae310bc
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Mon May 29 21:49:10 2023 +1200
    
        add health part in respawn
    
        add health part in respawn
    
    commit 5f70463a6166870a95a791199487d265f2d2b4c2
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Mon May 29 18:57:27 2023 +1200
    
        keep develop the respawn
    
        keep build the respawn
    
    commit f5ca9e3527f6e7cf381f20eb577398807ab2218c
    Merge: 415a21a d1184a7
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Sun May 28 17:44:40 2023 +1200
    
        Merge branch 'main' into Kevin---PlayerRespawn
    
    commit 415a21a3f68d728c97aaa050c239fefdf5ad0235
    Merge: 2e5ada1 a51ec2b
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Fri May 26 21:05:51 2023 +1200
    
        Merge branch 'main' into Kevin---PlayerRespawn
    
    commit 2e5ada1226ba7155fc1e9bf1070dce22eb3f3ff5
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Fri May 26 21:01:08 2023 +1200
    
        FIX SOME map problem
    
        FIX SOME map problem
    
    commit 7b441eb382bda9ac00edaf9bd67b17d1c001831f
    Merge: a7511a6 54cfe3e
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Fri May 26 20:46:20 2023 +1200
    
        Merge branch 'Kevin---PlayerRespawn' of https://github.com/dgw7626/COMP602_1_OrangeKoch into Kevin---PlayerRespawn
    
    commit a7511a64aa9050cf67eb4590635ac98c5c69088f
    Merge: c492ea6 c290e26
    Author: persona2490 <114039152+persona2490@users.noreply.github.com>
    Date:   Fri May 26 19:55:18 2023 +1200
    
        Merge branch 'main' into Kevin---PlayerRespawn

--- SDP_Main/Assets/Scripts/Game/GameModes/GameMode_Standard.cs
+++ SDP_Main/Assets/Scripts/Game/GameModes/GameMode_Standard.cs
@@ -11,7 +11,8 @@ using Photon.Realtime;
 /// 
 /// This is a native c# class, and is held statically by game_RuntimeData. Implements the IgameMode interface.
 /// </summary>
-public class GameMode_Standard : IgameMode
+// public class GameMode_Standard : IgameMode
+public class GameMode_Standard : MonoBehaviour, IgameMode
 {
     public const int MAX_GAME_TIME_SECONDS = 120;
     private const int NUM_TEAMS = 2;
@@ -23,9 +24,20 @@ public class GameMode_Standard : IgameMode
     /// Called by the gameModeManager instace from within the scene.
     /// Sets up the game and initializes values. 
     /// </summary>
+    // Current Change
+    // public void InitGame()
+    // {
+    //     numPlayers = 0;
+
+    // Kevin add: if make any problem, please delete that.
+    private Player_Health playerHealth;
+    private Weapon_ProjectileManager weapon_ProjectileManager;
+    private Coroutine Coroutine;
     public void InitGame()
     {
-        numPlayers = 0;
+        teamScores = new s_GameScore();
+        teamScores.killsPerTeam = new List<int>();
+
         // Make teams
         for (int i = 0; i < NUM_TEAMS; i++)
         {
@@ -64,7 +76,7 @@ public class GameMode_Standard : IgameMode
         // Initialize Countdown
         if (PhotonNetwork.IsMasterClient)
         {
-            GameMode_Manager.gameTime =  MAX_GAME_TIME_SECONDS;
+            GameMode_Manager.gameTime = MAX_GAME_TIME_SECONDS;
         }
 
         StartGame();
@@ -81,15 +93,18 @@ public class GameMode_Standard : IgameMode
         foreach (Player_MultiplayerEntity p in Game_RuntimeData.instantiatedPlayers)
         {
             p.playerController.IsInputLocked = false;
-      
-            if(p.playerController.photonView.IsMine)
+
+            if (p.playerController.photonView.IsMine)
             {
                 Game_RuntimeData.thisMachinesPlayersPhotonView = p.playerController.photonView;
-                Debug.Log("At START GAME: ThisMachines PhotonView is mine, and my number is: " + PhotonNetwork.LocalPlayer.ActorNumber + 
-                    " " + Game_RuntimeData.thisMachinesPlayersPhotonView.Owner.ActorNumber);
+                Debug.Log(
+                    "At START GAME: ThisMachines PhotonView is mine, and my number is: "
+                        + PhotonNetwork.LocalPlayer.ActorNumber
+                        + " "
+                        + Game_RuntimeData.thisMachinesPlayersPhotonView.Owner.ActorNumber
+                );
             }
         }
-
     }
 
     /// <summary>
@@ -111,6 +126,16 @@ public class GameMode_Standard : IgameMode
         {
             p.playerController.IsInputLocked = true;
         }
+        // Current changes
+        // if (Game_RuntimeData.thisMachinesPlayersPhotonView.Owner.IsMasterClient)
+        // {
+        //     Game_RuntimeData.thisMachinesPlayersPhotonView.RPC(
+        //         nameof(Player_MultiplayerEntity.OnGameEnded),
+        //         RpcTarget.All,
+        //         JsonUtility.ToJson(teamScores)
+        //     );
+        // }
+
         //TODO: Cleanup
 
         s_GameScore score = Game_RuntimeData.gameScore;
@@ -128,20 +153,35 @@ public class GameMode_Standard : IgameMode
     public IEnumerator OnOneSecondCountdown()
     {
         Debug.Log("Begin! ");
-        while(GameMode_Manager.timerIsRunning)
+
+        // Current Change
+        // while(GameMode_Manager.timerIsRunning)
+
+        while (GameMode_Manager.gameIsRunning)
         {
-            if(PhotonNetwork.IsMasterClient)
+            if (PhotonNetwork.IsMasterClient)
             {
                 GameMode_Manager.SetSynchronousTimerValue();
-                Game_RuntimeData.thisMachinesPlayersPhotonView.RPC("GetSynchronousTimerValue", RpcTarget.Others, GameMode_Manager.gameTime);
+                Game_RuntimeData.thisMachinesPlayersPhotonView.RPC(
+                    "GetSynchronousTimerValue",
+                    RpcTarget.Others,
+                    GameMode_Manager.gameTime
+                );
 
+                // If the timer expires, tell the other players what the score is.
+                if (GameMode_Manager.gameTime < 1)
+                {
+                    Game_RuntimeData.thisMachinesPlayersPhotonView.RPC(
+                        nameof(Player_MultiplayerEntity.OnGameEnded),
+                        RpcTarget.Others,
+                        JsonUtility.ToJson(Game_RuntimeData.gameScore)
+                    );
+
+                    GameMode_Manager.gameIsRunning = false;
+                }
             }
 
-            if(GameMode_Manager.gameTime < 1)
-            {
-                GameMode_Manager.timerIsRunning = false;
-            }
-
+            //Debug.Log(GameMode_Manager.gameTime);
             yield return new WaitForSeconds(1);
         }
 
@@ -152,9 +192,14 @@ public class GameMode_Standard : IgameMode
     /// <summary>
     /// Use for any per-frame game logic.
     /// </summary>
-    public void OnPerFrameUpdate()
+    public void OnPerFrameUpdate() { }
+
+    public void OnScoreEvent(int score, int teamNumber)
     {
-    }
+        if (teamNumber > NUM_TEAMS || teamNumber < 0)
+            Debug.LogError(
+                "ERROR: Team " + teamNumber + " does not exist! Cannot assign points to team"
+            );
 
     /// <summary>
     /// Only the Master Client will execute this method. Counts score locally, to be shared with others later.
@@ -175,9 +220,25 @@ public class GameMode_Standard : IgameMode
     /// Executed by everyone. This tells a machine that a particular player has been killed, so you should handle respawning if applicable.
     /// </summary>
     /// <param name="deathInfoStruct"></param>
+        
     public void OnPlayerKilled(s_DeathInfo deathInfoStruct)
     {
-        
+        //TODO: Find player and respwan/destroy them here
+        foreach (
+            KeyValuePair<int, Player_MultiplayerEntity> value in Game_RuntimeData.activePlayers
+        )
+        {
+            if (value.Key == deathInfoStruct.diedId)
+            {
+                OnPlayerRespawn(value);
+                return;
+            }
+        }
+    }
+
+    public void LeaveScene(string sceneName)
+    {
+        Game_GameState.NextScene(sceneName);
     }
 
     /// <summary>
@@ -205,10 +266,52 @@ public class GameMode_Standard : IgameMode
         {
             if (e.GetComponent<PhotonView>().Owner.ActorNumber == id)
             {
-                Game_RuntimeData.RegisterNewMultiplayerPlayer(e.GetComponent<PhotonView>().Owner.ActorNumber, e);
+                Game_RuntimeData.RegisterNewMultiplayerPlayer(
+                    e.GetComponent<PhotonView>().Owner.ActorNumber,
+                    e
+                );
             }
         }
     }
 
 
+    /// <summary>
+    /// Respawns the player and performs health and ammunition updates.
+    /// </summary>
+    /// <param name="value">Key-value pair representing the player's unique identifier and corresponding multiplayer entity.</param>
+    public void OnPlayerRespawn(KeyValuePair<int, Player_MultiplayerEntity> value)
+    {
+
+        Player_Health playerHealth = value.Value.GetComponent<Player_Health>();
+        Weapon_ProjectileManager weapon_ProjectileManager = value.Value.gameObject.GetComponentInChildren<Weapon_ProjectileManager>();
+        //update health
+        playerHealth.currentHealth = playerHealth.maxHealth;
+        playerHealth.currentUIHealth = playerHealth.maxHealth;
+        playerHealth.healthBar.SetHealth(playerHealth.currentUIHealth);
+        //Update the ammunition
+
+        weapon_ProjectileManager._weaponAmmo = weapon_ProjectileManager._weaponInfo.BulletCounts;
+        weapon_ProjectileManager._weaponClip = weapon_ProjectileManager._weaponInfo.ClipCounts;
+
+        weapon_ProjectileManager._ammunitionUI.SetAmmunition(
+             weapon_ProjectileManager._weaponAmmo,
+             weapon_ProjectileManager._weaponClip
+         );
+        // update the respawn point
+        value.Value.gameObject.transform.position = new Vector3(0, 30, 0);
+        //set the invincible time
+        Player targetPlayer = null;
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            if (p.ActorNumber == value.Key)
+            {
+                targetPlayer = p;
+                break;
+            }
+        }
+        //Call the "OnRespawn" method on the target player's multiplayer entity via RPC
+        if (targetPlayer != null)
+            if (targetPlayer != null)
+                Game_RuntimeData.thisMachinesPlayersPhotonView.RPC(nameof(Player_MultiplayerEntity.OnRespawn), targetPlayer);
+    }
 }
