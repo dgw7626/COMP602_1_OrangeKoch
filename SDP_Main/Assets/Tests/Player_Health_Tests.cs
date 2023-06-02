b20ee5949a09c06aa6a9925020b28e67437a56ec
persona2490 <114039152+persona2490@users.noreply.github.com>
2023-06-02 13:35:29 +1200

    Refactor player health test

--- SDP_Main/Assets/Tests/Player_Health_Tests.cs
+++ SDP_Main/Assets/Tests/Player_Health_Tests.cs
@@ -29,7 +29,7 @@ public class Player_Health_Tests
         // ARRANGE
         s_DamageInfo damageInfo = new s_DamageInfo();
 
-        float damageValue = -50.0f;
+        float damageValue = 50.0f;
         damageInfo.dmgValue = damageValue;
         float expectedHealth = playerHealth.currentHealth - damageValue;
         // ACT
@@ -40,19 +40,12 @@ public class Player_Health_Tests
     }
 
     [UnityTest]
-    /// <summary>
-    /// Coroutine that updates the UI health value over time.
-    /// </summary>
-    /// <returns>An IEnumerator used for the coroutine.</returns>
     public IEnumerator UpdateUI_HealthDecreasesOverTime()
     {
-        // Set the current UI health value
+        // ARRANGE
         playerHealth.currentUIHealth = playerHealth.maxHealth;
-
-        // Set the wait time for the coroutine
         playerHealth.UI_HealthTime = 0.16f;
-
-        // Start the coroutine
+        // ACT
         updateUICoroutine = playerHealth.StartCoroutine(playerHealth.UpdateUI());
 
         float elapsedTime = 0f;
@@ -61,14 +54,23 @@ public class Player_Health_Tests
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-
-        // Get the updated current UI health value
         float updatedUIHealth = playerHealth.currentUIHealth;
 
-        // Validate if the health value has decreased, indicating that the UI has been updated
+        // ASSERT
         Assert.Less(updatedUIHealth, 100.0f);
 
         // Stop the coroutine
         playerHealth.StopCoroutine(updateUICoroutine);
+    }
+
+    [Test]
+    public void RespawnTest() { 
+        //ARRANGE
+        playerHealth.currentHealth = 0;
+        //ACT
+        playerHealth.Respawn();
+        //Assert
+        Assert.AreEqual(playerHealth.currentHealth, 100.0f);
+
     }
 }
